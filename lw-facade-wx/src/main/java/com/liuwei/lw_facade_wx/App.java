8ea9a69ebeddc56bc7a2f2685a6176b0f38cccd2
hcliuwei <hcliuwei@qq.com>
2017-01-06 16:01:05 +0800

    test

---
+++ lw-facade-wx/src/main/java/com/liuwei/lw_facade_wx/App.java
@@ -0,0 +1,13 @@
+package com.liuwei.lw_facade_wx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ lw-service-wx/src/main/java/com/liuwei/lw_service_wx/App.java
@@ -0,0 +1,13 @@
+package com.liuwei.lw_service_wx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
